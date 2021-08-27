458ff3f73b9069202f6fef5d589a385369e161d9
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2021-08-27 11:37:44 +0200

    commit

---
+++ src/consoleapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using System;
+
+namespace DatadogConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+            var logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    new Serilog.Formatting.Json.JsonFormatter(renderMessage: true), 
+                    "logs/datadog-console-app.txt", 
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            logger.Information("Starting app...");
+
+        }
+    }
+}
---
+++ src/webapi/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+namespace DatadogWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                 .ReadFrom.Configuration(Configuration)
+                 .Enrich.FromLogContext()
+                 .WriteTo.Console()
+                 .WriteTo.File(new Serilog.Formatting.Json.JsonFormatter(renderMessage: true), "logs/webapi.txt", rollingInterval: RollingInterval.Day)
+                 .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Web Host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+    }
+}
