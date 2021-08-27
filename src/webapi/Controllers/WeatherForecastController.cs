e4b2af90aa3f73ce1ec46315e8ce061d4455c07b
takeshelter <35832531+sheltertake@users.noreply.github.com>
2021-08-27 11:38:24 +0200

    commit (#1)
    Co-authored-by: Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>

---
+++ src/webapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatadogWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            _logger.LogInformation("Getting WeatherForecast...");
+
+            using (var scope = Datadog.Trace.Tracer.Instance.StartActive($"{nameof(WeatherForecastController)}.{nameof(Get)}"))
+            {
+                _logger.LogInformation("WeatherForecast log and trace.");
+            }
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("myaction")]
+        public ActionResult MyAction()
+        {
+            return Ok();
+        }
+    }
+}
